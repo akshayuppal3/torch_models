f13c7a3b32f0eebe0b86fce038f0eb064dc554c5
akshayu <akshay.uppal@imanage.com>
2020-10-02 10:08:32 -0500

    addition of pytorch models

--- linear_regression/linear_model.py
+++ linear_regression/linear_model.py
@@ -3,10 +3,12 @@ import torch.nn as nn
 
 
 class ManualRegression(nn.Module):
+
     def __init__(self):
         super().__init__()
-        self.a = nn.Parameter(torch.randn(1, requires_grad=True, dtype=torch.float))
-        self.b = nn.Parameter(torch.randn(1, requires_grad=True, dtype=torch.float))
+        # self.a = nn.Parameter(torch.randn(1, requires_grad=True, dtype=torch.float))
+        # self.b = nn.Parameter(torch.randn(1, requires_grad=True, dtype=torch.float))
+        self.parameters = nn.Linear(1, 1) # basic linear pytoch layer
 
     def forward(self, x):
         return self.a + self.b * x
---
+++ readme.md
@@ -0,0 +1,5 @@
+## Torch models
+
+Implementation of some basic models in pytorch
+
+- Linear Regression
